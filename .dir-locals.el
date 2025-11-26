5a5acf688048d232768799221766f933112ea44a
Dietrich Daroch <Dietrich@Daroch.me>
2025-11-26 19:16:55 -0300

    Misc,Emacs: Set projectile-project compilation/install commands

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables         -*- no-byte-compile: t; -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+(
+ (nil . (
+				 (projectile-project-compilation-cmd . "make build")
+				 (projectile-project-install-cmd     . "make export")
+				 ))
+ )
